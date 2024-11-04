d5e970d200afa3d5ce24d2735055be24e0821052
cmotuzas <cmotuzas@uwo.ca>
2024-11-04 13:58:12 -0500

    Update Project2-Alpha.py

--- Project2-Alpha.py
+++ Project2-Alpha.py
@@ -35,8 +35,20 @@ y0 = 1.0 m'''
 mu_v0 = 100 
 std_v0 = 15
 size0 = (1,)
-v0 = np.random.normal(mu_v0, std_v0,size0)
+v0 = np.random.normal(mu_v0, std_v0,size0)/2.237 # divided by 2.237 to obtain value in m/s rather than mph
 
 mu_theta_0 = 45
 std_theta_0 = 10 
 theta_0 = np.random.normal(mu_theta_0,std_theta_0,size0)
+
+# tau = 
+
+# Euler Method
+
+
+
+# Euler-Cromer Method 
+
+
+
+# Midpoint Method 
