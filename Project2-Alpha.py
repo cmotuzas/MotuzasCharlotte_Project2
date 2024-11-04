cab04f729d8bd44874a693d7175cd26a9f01bb01
cmotuzas <cmotuzas@uwo.ca>
2024-11-04 14:35:01 -0500

    Create Project2-Alpha.py

---
+++ Project2-Alpha.py
@@ -0,0 +1,10 @@
+# Project 2 - Charlotte Motuzas 
+# Due Nov. 8th, 2024 
+# Physics 3926 
+
+import numpy as np 
+import scipy as scp 
+import matplotlib.pyplot as plt 
+import scipy.integrate as scint 
+
+# TESTING TESTING TESTING 
